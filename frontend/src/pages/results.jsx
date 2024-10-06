e612e424b071dc4f88d125eb20319b99ea5149c2
richardyang22 <yang.richard22@gmail.com>
2024-10-05 20:43:05 -0400

    add results page

--- frontend/src/pages/results.jsx
+++ frontend/src/pages/results.jsx
@@ -1,10 +1,45 @@
-import { useLocation } from 'react-router-dom'
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useLocation } from 'react-router-dom';
+import './results.css';
 
 export function ResultsPage() {
     const { image } = useLocation().state;
+    const data = [
+        { label: 'Steak', value: 35 },
+        { label: 'Potatoes', value: 3 },
+        { label: 'Asparagus', value: 2 },
+    ];
+    const total = data.reduce((acc, curr) => acc + curr.value, 0);
     return (
-        <div>
-            <img src={URL.createObjectURL(image)} />
+        <div className="results-container">
+            <img src={URL.createObjectURL(image)} width="400px" />
+            <PieChart
+                series={[
+                    {
+                        data: data,
+                        arcLabel: params => params.label,
+                    },
+                ]}
+                width={500}
+                height={300}
+                slotProps={{
+                    legend: { hidden: true },
+                }}
+            />
+            <table className="legend">
+                <tr>
+                    <th>Food Item</th>
+                    <th>kg of CO2</th>
+                    <th>Percentage</th>
+                </tr>
+                {data.map(item => (
+                    <tr key={item.label}>
+                        <td>{item.label}</td>
+                        <td>{item.value}</td>
+                        <td>{(item.value / total) * 100}%</td>
+                    </tr>
+                ))}
+            </table>
         </div>
     );
 }
