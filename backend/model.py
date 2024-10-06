eae6617f2fae3498c9c1fd97d12359e411171604
arieycliu <ariel.yc.liu@gmail.com>
2024-10-06 05:17:05 -0400

    test backend

--- backend/model.py
+++ backend/model.py
@@ -11,19 +11,19 @@ def predict(image_name):
 
     for result in results:
     
-        boxes = result.boxes.xyxy  # Bounding boxes in (x1, y1, x2, y2) format
-        confidences = result.boxes.conf  # Confidence scores
-        class_ids = result.boxes.cls  # Class IDs
+    #     boxes = result.boxes.xyxy  # Bounding boxes in (x1, y1, x2, y2) format
+    #     confidences = result.boxes.conf  # Confidence scores
+    #     class_ids = result.boxes.cls  # Class IDs
 
-        for box, conf, cls in zip(boxes, confidences, class_ids):
-            x1, y1, x2, y2 = box
-            label = f"Class: {class_names[str(int(cls))]}, Confidence: {conf:.2f}"
-            cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            cv2.putText(image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    #     for box, conf, cls in zip(boxes, confidences, class_ids):
+    #         x1, y1, x2, y2 = box
+    #         label = f"Class: {class_names[str(int(cls))]}, Confidence: {conf:.2f}"
+    #         cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+    #         cv2.putText(image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        # Display the result
-        output_filepath = f'images_out/{image_name}'
-        cv2.imwrite(output_filepath, image)
+    #     # Display the result
+    #     output_filepath = f'images_out/{image_name}'
+    #     cv2.imwrite(output_filepath, image)
 
         detected_classes = set()
         for result in results:
